354a2802559d9163da2800a640f159817ed4bc47
preciousimo <preciousimoniakemu@gmail.com>
2022-06-02 20:18:00 +0100

    Setup project structure

--- App/views.py
+++ App/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Fuction to home page (Frontend)
+def home(request):
+    return render(request, "home.html")
+
+# Fuction to inbox (Backend)
+def inbox(request):
+    return render(request, "inbox.html")
