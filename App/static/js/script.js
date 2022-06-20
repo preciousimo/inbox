e58b44a8035a95ee4c9951714bba0a84656fd062
preciousimo <preciousimoniakemu@gmail.com>
2022-06-20 18:11:58 +0100

    Function to hide/show password

--- App/static/js/script.js
+++ App/static/js/script.js
@@ -4,3 +4,14 @@ $(document).ready(function() {
         $('.text-pulse').fadeOut(1000).fadeIn(1000, pulse);
     })();
 });
+
+// Function to hide/show the password
+function myFunction() {
+    var p = document.getElementById("password");
+    if (p.type === "password") {
+        p.type = "text";
+    }
+    else {
+        p.type = "password"
+    }
+}
