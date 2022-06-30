a08238afb01c912811216c286ad118fcb9563eb1
preciousimo <preciousimoniakemu@gmail.com>
2022-06-30 12:50:14 +0100

    Custom Django admin

--- inbox/urls.py
+++ inbox/urls.py
@@ -4,9 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # App urls
     path('', include('App.urls')),
+
     # Path to Login/Logout
     path('inbox/', include('django.contrib.auth.urls')),
+
+    # Django JET URLS
+    path('jet/', include('jet.urls', 'jet')),
+    path('jet/dashboard', include('jet.dashboard.urls', 'jet-dashboard')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
