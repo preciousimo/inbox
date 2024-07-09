f43e833693f9daf34050fbf8eac58021600ddc4e
preciousimo <preciousimoniakemu@gmail.com>
2024-07-09 06:20:12 +0100

    "Updated forms.py: added MultipleClearableFileInput class, modified Customerform and EmailForm"

--- App/forms.py
+++ App/forms.py
@@ -1,6 +1,10 @@
-# from email import message
 from django import forms
 from .models import Customer
+
+class MultipleClearableFileInput(forms.FileInput):
+    def __init__(self, attrs=None, **kwargs):
+        super().__init__(attrs, **kwargs)
+        self.attrs['multiple'] = True
 
 class Customerform(forms.ModelForm):
     file = forms.FileField(required=False)
@@ -8,10 +12,12 @@ class Customerform(forms.ModelForm):
         model = Customer
         fields = "__all__"
 
-# Reply emails
 class EmailForm(forms.Form):
     email = forms.EmailField()
     cc = forms.EmailField(required=False)
     subject = forms.CharField(max_length=100)
-    attach = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    attach = forms.FileField(
+        required=False,
+        widget=MultipleClearableFileInput()
+    )
     message = forms.CharField(widget=forms.Textarea)
